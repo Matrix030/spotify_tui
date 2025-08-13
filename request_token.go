88469c48ff72ebbdf3323c0e21db4802c8a917e7
Matrix030 <rishigharat@gmail.com>
2025-08-13 08:36:37 -0400

    todo access tokens stuff

---
+++ request_token.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func requestToken(client_id string, client_secret string) tokenStruct {
+	accessToken := tokenStruct{}
+	data := url.Values{}
+	data.Set("grant-type", "client_credentials")
+	data.Set("client_id", client_id)
+	data.Set("client_secret", client_secret)
+
+	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", strings.NewReader(data.Encode()))
+	if err != nil {
+		panic(err)
+	}
+
+	return accessToken
+}
